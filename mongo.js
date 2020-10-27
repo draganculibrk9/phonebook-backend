8920151b389933a1d438e55681efc224ba519959
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-10-27 14:18:46 +0100

    Finish exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = encodeURI(process.argv[2])
+
+const url =
+    `mongodb+srv://dragan97:${password}@cluster0.dpahi.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    id: Number
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+if (name && number) {
+    const person = new Person({
+        name,
+        number,
+        id: Math.floor(Math.random() * 1000)
+    })
+
+    person.save().then(() => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
